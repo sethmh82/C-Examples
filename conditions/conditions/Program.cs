4811452a90e2769c5f1df0115179bca3eab94764
sethmh82 <seth@statisticbrain.com>
2017-05-20 10:10:12 -0700

    Updates

--- conditions/conditions/Program.cs
+++ conditions/conditions/Program.cs
@@ -2,11 +2,24 @@
 
 namespace conditions
 {
-	class MainClass
+	class Program
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			//Conditions
+
+			int x = int.Parse(Console.ReadLine());
+
+			if (x == 10)
+			{
+				Console.WriteLine("You typed ten");
+			}
+			else
+			{
+				Console.WriteLine("No ten here sorry bro");
+			}
+
+			Console.ReadLine();
 		}
 	}
 }
